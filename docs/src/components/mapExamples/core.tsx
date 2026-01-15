0381b5046c74af0b38188cb61782db808f672e90
Haneul Jin <hnjin@citus.co.kr>
2026-01-15 15:54:59 +0900

    Update map examples to enhance shape drawing functionality, allowing users to add points on the map to create polylines and polygons. Modify package.json to change the default port for the Docusaurus start script.

--- docs/src/components/mapExamples/core.tsx
+++ docs/src/components/mapExamples/core.tsx
@@ -99,17 +99,24 @@ export function PolygonExample({
   ncpKeyId = defaultNcpKeyId,
 }: MapExampleProps) {
   const paths: naver.maps.ArrayOfCoordsLiteral[] = [
-    [{ x: 127.0276, y: 37.4979 }],
-    [{ x: 127.0286, y: 37.4979 }],
-    [{ x: 127.0286, y: 37.4989 }],
-    [{ x: 127.0276, y: 37.4989 }],
+    [
+      { y: 37.37544345085402, x: 127.11224555969238 },
+      { y: 37.37230584065902, x: 127.10791110992432 },
+      { y: 37.35975408751081, x: 127.10795402526855 },
+      { y: 37.359924641705476, x: 127.11576461791992 },
+      { y: 37.35931064479073, x: 127.12211608886719 },
+      { y: 37.36043630196386, x: 127.12293148040771 },
+      { y: 37.36354029942161, x: 127.12310314178465 },
+      { y: 37.365211629488016, x: 127.12456226348876 },
+      { y: 37.37544345085402, x: 127.11224555969238 },
+    ],
   ];
 
   return (
     <div style={{ width: "100%", height: "400px", margin: "20px 0" }}>
       <Map
         ncpKeyId={ncpKeyId}
-        mapOptions={{ center: { x: 127.0281, y: 37.4984 }, zoom: 15 }}
+        mapOptions={{ center: { x: 127.1137238, y: 37.3664975 }, zoom: 13 }}
       >
         <Polygon
           paths={paths}
@@ -137,7 +144,7 @@ export function PolylineExample({
     <div style={{ width: "100%", height: "400px", margin: "20px 0" }}>
       <Map
         ncpKeyId={ncpKeyId}
-        mapOptions={{ center: { x: 127.0286, y: 37.4989 }, zoom: 14 }}
+        mapOptions={{ center: { x: 127.0286, y: 37.4989 }, zoom: 15 }}
       >
         <Polyline
           path={path}
--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -1,60 +1,135 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef, useState } from "react";
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import { useMap } from "./MapProvider";
 
-export interface PolygonProps extends naver.maps.PolygonOptions {
-  onClick?: PolygonCallbacks["click"];
-  onMouseEnter?: PolygonCallbacks["mouseenter"];
-}
+const EVENT_TO_PROP = {
+  click: "onClick",
+  dblclick: "onDblclick",
+  rightclick: "onRightclick",
+  mousedown: "onMouseDown",
+  mouseup: "onMouseUp",
+  mouseover: "onMouseOver",
+  mouseout: "onMouseOut",
+  mouseenter: "onMouseEnter",
+  mouseleave: "onMouseLeave",
+} as const;
+
+type EventKey = keyof typeof EVENT_TO_PROP;
+type PropKey = (typeof EVENT_TO_PROP)[EventKey];
 
 interface PolygonCallbacks {
-  click?: (event: unknown) => void;
-  mouseenter?: (event: unknown) => void;
+  click?: (event: naver.maps.PointerEvent) => void;
+  dblclick?: (event: naver.maps.PointerEvent) => void;
+  rightclick?: (event: naver.maps.PointerEvent) => void;
+  mousedown?: (event: naver.maps.PointerEvent) => void;
+  mouseup?: (event: naver.maps.PointerEvent) => void;
+  mouseover?: (event: naver.maps.PointerEvent) => void;
+  mouseout?: (event: naver.maps.PointerEvent) => void;
+  mouseenter?: (event: naver.maps.PointerEvent) => void;
+  mouseleave?: (event: naver.maps.PointerEvent) => void;
 }
-const Polygon = ({
-  onClick = () => {},
-  onMouseEnter = () => {},
-  ...options
-}: PolygonProps) => {
-  const { current: map } = useMap();
-  const [instance, setInstance] = useState<naver.maps.Polygon | null>(null);
-  const callbacksRef = useRef<PolygonCallbacks>({});
 
-  useEffect(() => {
-    callbacksRef.current = {
-      click: onClick,
-      mouseenter: onMouseEnter,
-    };
-  }, [onClick]);
+type HandlerOfProp<P extends PropKey> = PolygonCallbacks[{
+  [E in EventKey]: (typeof EVENT_TO_PROP)[E] extends P ? E : never;
+}[EventKey]];
 
-  useEffect(() => {
-    if (!instance) return;
-    const handlers: naver.maps.MapEventListener[] = [];
+type PolygonEventProps = {
+  [P in PropKey]?: HandlerOfProp<P>;
+};
 
-    Object.entries(callbacksRef.current).forEach(([key, value]) => {
-      if (!value) return;
-      handlers.push(instance.addListener(key, value));
+type BasePolygonProps = Omit<naver.maps.PolygonOptions, "map">;
+
+export interface PolygonProps extends BasePolygonProps, PolygonEventProps {}
+
+const Polygon = forwardRef<naver.maps.Polygon, PolygonProps>(
+  (props: PolygonProps, ref: React.Ref<naver.maps.Polygon | undefined>) => {
+    const { current: map } = useMap();
+    const [instance, setInstance] = useState<naver.maps.Polygon>();
+
+    const eventProps: PolygonEventProps = {};
+    const options: Partial<naver.maps.PolygonOptions> = {};
+
+    (Object.keys(props) as Array<keyof PolygonProps>).forEach((key) => {
+      const propKey = key as PropKey;
+      if (
+        Object.values(EVENT_TO_PROP).includes(
+          propKey as (typeof EVENT_TO_PROP)[EventKey]
+        )
+      ) {
+        const value = props[propKey];
+        if (value !== undefined) {
+          (eventProps as Record<string, unknown>)[propKey] = value;
+        }
+      } else {
+        const value = props[key];
+        if (value !== undefined) {
+          (options as Record<string, unknown>)[key] = value;
+        }
+      }
     });
 
-    return () => {
-      handlers.forEach((handler) => instance.removeListener(handler));
-    };
-  }, [instance]);
+    useEffect(() => {
+      if (!map) return undefined;
+      const newInstance = new naver.maps.Polygon(
+        options as naver.maps.PolygonOptions
+      );
+      setInstance(newInstance);
+      newInstance.setMap(map);
+      return () => {
+        if (!map) return;
+        newInstance.setMap(null);
+      };
+    }, [map]);
 
-  useEffect(() => {
-    if (!map) return;
-    const newInstance = new naver.maps.Polygon(options);
-    newInstance.setMap(map);
-    setInstance(newInstance);
-    return () => {
-      newInstance.setMap(null);
-    };
-  }, [map]);
+    useEffect(() => {
+      if (!instance) return;
+      instance.setOptions(options as naver.maps.PolygonOptions);
+    }, [instance, options]);
 
-  useEffect(() => {
-    instance?.addListener("click", onClick);
-  }, []);
-  return null;
-};
+    const handlersRef = useRef<
+      Partial<Record<EventKey, (...args: unknown[]) => void>>
+    >({});
+
+    useEffect(() => {
+      const next: Partial<Record<EventKey, (...args: unknown[]) => void>> = {};
+      (Object.keys(EVENT_TO_PROP) as EventKey[]).forEach((eventKey) => {
+        const propKey = EVENT_TO_PROP[eventKey];
+        const fn = (eventProps as Record<string, unknown>)[propKey];
+        if (typeof fn === "function")
+          next[eventKey] = fn as (...args: unknown[]) => void;
+      });
+      handlersRef.current = next;
+    }, [eventProps]);
+
+    useEffect(() => {
+      if (!instance) return;
+
+      const disposers: Array<() => void> = [];
+      (Object.keys(EVENT_TO_PROP) as EventKey[]).forEach((eventKey) => {
+        const listener = naver.maps.Event.addListener(
+          instance,
+          eventKey,
+          (...args: unknown[]) => {
+            handlersRef.current[eventKey]?.(...args);
+          }
+        );
+        disposers.push(() => naver.maps.Event.removeListener(listener));
+      });
+      return () => disposers.forEach((d) => d());
+    }, [instance]);
+
+    useImperativeHandle(ref, () => instance, [instance]);
+
+    return null;
+  }
+);
+
+Polygon.displayName = "Polygon";
 
 export default Polygon;
